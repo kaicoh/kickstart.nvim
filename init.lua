142de6fd68b6edfd55a727b3418965fd8adb005a
Kanji Tanaka <sumireminami@gmail.com>
2026-05-14 11:01:05 +0900

    Adjust 60 keyboard seconday mode

--- init.lua
+++ init.lua
@@ -229,7 +229,8 @@ do
   -- TIP: Disable arrow keys in normal mode
   vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
   vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-  vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+  vim.keymap.set('n', '<up>', '/')
+  vim.keymap.set('n', '<S-Up>', '?')
   vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
   -- Split windows
